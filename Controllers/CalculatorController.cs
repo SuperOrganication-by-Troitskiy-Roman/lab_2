8914971e594f71257269d7b05a418ba91eafc45a
Roman <roman.troitskiy@icloud.com>
2025-04-16 18:54:53 +0700

    first commit

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(double num1, double num2, string operation)
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        return BadRequest("Ошибка: деление на ноль");
+                    }
+                    break;
+                default:
+                    return BadRequest("Неизвестная операция");
+            }
+
+            return Json(new { result });
+        }
+    }
+}
---
+++ Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class TestController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Greet(string name)
+    {
+        return Content($"Привет, {name}!");
+    }
+
+    public IActionResult UserInfo()
+    {
+        var user = new User { Name = "Иван Иванов", Age = 30 };
+        return View(user);
+    }
+}
